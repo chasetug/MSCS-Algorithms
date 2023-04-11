e8ee0b37a49da51447321e46f0d8d6e9c40d40c8
halfhand870 <send2lukew@gmail.com>
2023-04-11 16:41:39 -0500

    Added main method

--- MSCS-Algorithms/Program.cs
+++ MSCS-Algorithms/Program.cs
@@ -4,13 +4,20 @@ namespace MSCS_Algorithms;
 
 public class Program
 {
+    static void Main(string[] args)
+    {
+
+    }
+
     public static List<List<int>> GenerateRandomArrays()
     {
         return new List<List<int>>();
     }
 
+    // We will be reading a file named "phw_input.txt"
     public static int[] ReadFile(string path)
     {
+
         return new int[0];
     }
 }

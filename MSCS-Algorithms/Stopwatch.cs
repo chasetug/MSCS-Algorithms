9a6442c0ea7530b2357e52fc428c00653c44710d
halfhand870 <send2lukew@gmail.com>
2023-04-12 16:13:48 -0500

    Made the stopwatch

---
+++ MSCS-Algorithms/Stopwatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MSCS_Algorithms;
+
+public class Stopwatch
+{
+    // Sets up current time, pushes in current time
+    static DateTime currentTime = DateTime.Now;
+    
+    // Marks the current time
+    // -> Outputs the time difference if reset is false
+    public static void MarkTime(bool reset = false)
+    {
+        // -> If reset is true, set current time and return
+        if (reset)
+        {
+            currentTime = DateTime.Now;
+            return;
+        }
+
+        // Output the time difference
+        Console.WriteLine($"Time Taken: {DateTime.Now - currentTime}");
+        // Reset current
+        currentTime = DateTime.Now;
+    }
+}

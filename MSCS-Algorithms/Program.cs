efba8a71d1553a85a723f1cf622b48e776ccb5b6
halfhand870 <send2lukew@gmail.com>
2023-04-12 14:36:02 -0500

    Finished Read File!

--- MSCS-Algorithms/Program.cs
+++ MSCS-Algorithms/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace MSCS_Algorithms;
+﻿using System.IO;
+
+namespace MSCS_Algorithms;
 
 public class Program
 {
+    static bool debug = true;
+
     static void Main(string[] args)
     {
+        // -> Debug write working directory
+        if (debug) Console.WriteLine("Working Directory: " + Directory.GetCurrentDirectory());
 
+        // Pull array from contained file
+        int[] pulledArr = ReadFile(Directory.GetCurrentDirectory() + "\\phw_input.txt");
+        // -> Debug write the second value to ensure that the array pulled successfully
+        if (debug) Console.WriteLine(pulledArr[1]);
     }
 
     public static List<List<int>> GenerateRandomArrays(int lower, int upper)
@@ -25,9 +35,78 @@ public class Program
     }
 
     // We will be reading a file named "phw_input.txt"
+    // -> File is comma denoted
+    // -> Will always be of length 10
     public static int[] ReadFile(string path)
     {
+        // Open up the file defined by path
+        StreamReader sr = new StreamReader(path);
+        // Pull contents and cut spaces
+        string fileContents = sr.ReadLine();
+        // -> If there are no contents the return
+        if (fileContents == null)
+            return new int[0];
+        // -> Cut spaces
+        fileContents = fileContents.Replace(" ", "");
+        // -> Debug write to console
+        if (debug) Console.WriteLine(fileContents);
 
-        return new int[0];
+        // Create the return array
+        int[] rArr = new int[10];
+
+        // Parse information into rArr
+        // -> Create an overflow value
+        int oVal = 0;
+        // Read information until there is nothing left in file contents
+        while(fileContents != string.Empty && oVal < 10)
+        {
+            // Pull substring of contents (between current and next comma
+            int commaIndex = fileContents.IndexOf(',');
+            // -> Pull parsed
+            string parseString = string.Empty;
+            if(commaIndex >= 0)
+            {
+                // -> Cut content
+                parseString = fileContents.Substring(0, commaIndex);
+                // -> Shorten file contents
+                fileContents = fileContents.Substring(commaIndex + 1);
+            }
+            else
+            {
+                // Set parse to remaining content
+                parseString = fileContents;
+                // Note file contents as empty
+                fileContents = string.Empty;
+            }
+
+            // Make sure value can be parsed
+            if(!int.TryParse(parseString, out rArr[oVal]))
+            {
+                Console.WriteLine($"Value '{parseString}' is not a valid integer... skipping");
+            }
+
+            // -> Debug out both strings
+            if (debug) Console.WriteLine($"PS: {parseString} || FC: {fileContents}");
+
+            // -> Increment oVal
+            oVal++;
+        }
+        // -> Send error if overflow was hit
+        if (fileContents != string.Empty)
+            Console.WriteLine($"File contains more than 10 values, parsing the first 10 values.\nValues left out: {fileContents}");
+
+        // Make sure to close the stream reader
+        sr.Close();
+
+        // -> Debug out rArr
+        if (debug)
+        {
+            Console.Write("| ");
+            for (int i = 0; i < rArr.Length; i++)
+                Console.Write(rArr[i] + " | ");
+            Console.Write("\n");
+        }
+
+        return rArr;
     }
 }
